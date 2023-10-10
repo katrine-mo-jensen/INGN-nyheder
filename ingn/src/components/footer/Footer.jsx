b1023ea14507eb7a575923a4b07ede6cf99d3c69
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-10-10 20:28:27 +0200

    mobile version done

--- ingn/src/components/footer/Footer.jsx
+++ ingn/src/components/footer/Footer.jsx
@@ -5,28 +5,28 @@ export const Footer = () => {
     <footer>
       <div className={style.footer}>
         <section className={style.adresse}>
-          <h6>Adresse</h6>
+          <h4>Adresse</h4>
           <p>Intet nyt - Godt nyt AsP</p>
           <p>
             Tulipanvej 232 <br /> 7320 <br /> Valby Øster
           </p>
         </section>
         <section className={style.links}>
-          <h6>Links</h6>
+          <h4>Links</h4>
           <a href="#">vikanweb.dk</a>
           <a href="#">overpådenandenside.dk</a>
           <a href="#">retsinformation.dk</a>
           <a href="#">nogetmednews.dk</a>
         </section>
         <section className={style.politik}>
-          <h6>Politik</h6>
+          <h4>Politik</h4>
           <a href="#">Privatlivspolitik</a>
           <a href="#">Cookiepolitik</a>
           <a href="#">Købsinformation</a>
           <a href="#">Delingspolitik</a>
         </section>
         <section className={style.kontakt}>
-          <h6>Kontakt</h6>
+          <h4>Kontakt</h4>
           <p>
             Email: ingn@nyhed.dk <br />
             Telefon: 23232323 <br />
