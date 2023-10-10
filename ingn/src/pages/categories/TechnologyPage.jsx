3e9b0e46dcf8a6893c4dac0ccd2b692b01fcba21
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-10-10 14:37:33 +0200

    Desktop version is done

--- ingn/src/pages/categories/TechnologyPage.jsx
+++ ingn/src/pages/categories/TechnologyPage.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { request } from "graphql-request";
 import { Link } from "react-router-dom";
 import { getTechnology } from "../../queries/technology";
+import style from "../../pages/grid.module.scss"
 
 export const TechnologyPage = () => {
   const { data, isLoading, error } = useQuery({
@@ -20,19 +21,44 @@ export const TechnologyPage = () => {
     return <p>Error: {error.message}</p>;
   }
   return (
-    <>
+    <section className={style.articleWrapper}>
       {data.ingnContents.map((item, index) => {
+        const classNames = [
+          style.art1,
+          style.art2,
+          style.art3,
+          style.art4,
+          style.art5,
+          style.art6,
+          style.art7,
+          style.art8,
+          style.art9,
+          style.art10,
+          style.art11,
+          style.art12,
+          // Add more class names as needed
+        ];
+
+        // Ensure that index does not go out of bounds of classNames array
+        const className = classNames[index % classNames.length];
+
         return (
-          <article key={index}>
-            <h2>{item.title}</h2>
-            <p>
-              D. {item.date} - af {item.author}
-            </p>
+          <article
+            key={index}
+            className={className}
+            style={{ gridArea: "article" + (index + 1) }}
+          >
+            <div>
+              <h2>{item.title}</h2>
+              <p>
+                D. {item.date} - af {item.author}
+              </p>
+              <Link to={`/${item.id}`}>Læs mere</Link>
+            </div>
             <img src={item.image.url} alt={item.descriptionOfImage} />
-            <Link to={`/${item.id}`}>Læs mere</Link>
           </article>
         );
       })}
-    </>
+    </section>
   );
 };
